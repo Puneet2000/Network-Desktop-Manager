ea91aa3c810b2c39a4a157d218b3eb4428795cf6
Puneet2000 <cs17btech11029@iith.ac.in>
2018-06-01 09:39:30 -0400

    Tcp connect scan modified

--- src/PortScanner.java
+++ src/PortScanner.java
@@ -2,8 +2,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import java.net.*;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
 import java.nio.channels.DatagramChannel;
 
 import javax.swing.*;
@@ -13,43 +11,97 @@ public class PortScanner {
 	public static void main (String args[]) {
 		new PortScanner().drawGUI();
 	}
-	public void TcpConnect(String ip) {
-		for (int i=0;i<=65535;i++) {
-			try {
-				Socket s = new Socket(ip,i);
-				System.out.println(ip+":"+i );
-				s.setSoTimeout(1000);
-				InputStream in = s.getInputStream();
-				BufferedInputStream bins = new BufferedInputStream(in);
-				BufferedReader bin = new BufferedReader(new InputStreamReader(bins));
-	            String response = bin.readLine();
-	            System.out.println(response);
-				s.close();
-			} catch (UnknownHostException e) {
-				System.out.println("Exc");
-				
-			}catch (SocketTimeoutException e) {
-				System.out.println("Timeout");
-				
-			}catch (IOException e) {
-				
-			}
+	public void TcpConnect(String ip)  {
+		int open =0;
+		for (int i=1;i<=65535;i++) {
+			String data="";
+			Socket socket = new Socket();
+			
+                try {
+                	socket.setReuseAddress(true);
+                	socket.setTcpNoDelay(true);
+	                socket.connect(new InetSocketAddress(ip, i), 6000);
+				} catch (ConnectException e) {
+					continue;
+				} catch (SocketTimeoutException e) {
+					
+						open++;
+		         	       System.out.println("Port "+Integer.toString(i)+" is filtered");
+		         	       continue;
+					
+				}
+                catch (IOException e) {
+                	e.printStackTrace();
+					continue;
+				}
+                
+                try {
+                    InputStreamReader input = new InputStreamReader(socket.getInputStream(), "UTF-8");
+                    BufferedReader buffered = new BufferedReader(input);
+                    if (i == 22) {
+                        data = parseSSH(buffered);
+                    } else if (i == 80 || i == 443 || i == 8080) {
+                        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+                        data = parseHTTP(buffered, out,ip);
+                    }
+                } catch (IOException e) {
+                	
+                } finally {
+                	open++;
+         	       System.out.println("Port "+Integer.toString(i)+" is open");
+         	       System.out.println(data);
+                    try {
+                        socket.close();
+                    } catch (IOException ignored) {
+                        // Something's really wrong if we can't close the socket...
+                    }
+    }
+	    }
+		System.out.println("Ports open " + Integer.toString(open));
 		}
-	}
+	
 	public void TcpSYN(String ip) {
 		
 	}
+	private String parseSSH(BufferedReader reader) throws IOException {
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
+        }
+}
+	private String parseHTTP(BufferedReader reader, PrintWriter writer,String ip) throws IOException {
+        writer.println("GET / HTTP/1.1\r\nHost: " + ip + "\r\n");
+        char[] buffer = new char[256];
+        reader.read(buffer, 0, buffer.length);
+        writer.close();
+        reader.close();
+        String data = new String(buffer).toLowerCase();
+
+        if (data.contains("apache") || data.contains("httpd")) {
+            return "Apache";
+        }
+
+        if (data.contains("iis") || data.contains("microsoft")) {
+            return "IIS";
+        }
+
+        if (data.contains("nginx")) {
+            return "NGINX";
+        }
+
+        return null;
+}
 	public void UdpScan(String ip) {
 		int open =0;
 		for (int i=1;i<=10;i++) {
-			DatagramSocket ds;
-	        DatagramPacket dp;
-	        DatagramChannel dChannel;
+			String response="";
 	        try{
-	            byte [] bytes = new byte[128];
-	            ds = new DatagramSocket();
-	            dp = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(ip),i);
-	            dChannel = DatagramChannel.open();
+	        	String m ="Hello";
+	            byte [] bytes = m.getBytes();
+	            DatagramSocket ds = new DatagramSocket();
+	            DatagramPacket dp = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(ip),i);
+	            DatagramChannel dChannel = DatagramChannel.open();
 	            dChannel.connect(new InetSocketAddress(ip, i));
 	            dChannel.configureBlocking(true);
 	            ds = dChannel.socket();
@@ -61,28 +113,28 @@ public class PortScanner {
 
 	            if (!dChannel.isConnected() || !dChannel.isOpen()){
 	                ds.close();
+	                continue;
 	            }
-	            System.out.println(Integer.toString(i) +" is open");
-	            String response = dp.toString();
-	            System.out.println(response);
+	            response = dp.toString();
 	            ds.disconnect();
 	            dChannel.disconnect();
 	            dChannel.close();
 	            ds.close();
 	        }
 	        catch(PortUnreachableException e){
-	        	 
+	        	 continue;
 	        }
 	        catch(InterruptedIOException e){
-	        	 
+	        	 continue;
 	        }
 	        catch(IOException e){
-	        	
+	        	continue;
 	        }
 	        catch(Exception e){
-	        	
+	        	continue;
 	        }
 	       open++;
+	       System.out.println(response);
 		}
 		System.out.println("Open ports : "+Integer.toString(open));
 	}
@@ -104,7 +156,9 @@ public class PortScanner {
 		tcpConnect.addActionListener(new ActionListener() { 
 			  public void actionPerformed(ActionEvent e) { 
 				  String ip = JOptionPane.showInputDialog("Enter IP address");
-				  TcpConnect(ip);
+				
+					TcpConnect(ip);
+				
 			   
 			  } 
 			  
