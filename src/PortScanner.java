449f8a4784942d58e816f44530c324b4d5ff5a81
Puneet2000 <cs17btech11029@iith.ac.in>
2018-06-01 03:20:42 -0400

    Udp port scanning added

--- src/PortScanner.java
+++ src/PortScanner.java
@@ -1,8 +1,11 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import java.net.Socket;
+import java.net.*;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.nio.channels.DatagramChannel;
+
 import javax.swing.*;
 import java.io.*;
 public class PortScanner {
@@ -25,7 +28,10 @@ public class PortScanner {
 			} catch (UnknownHostException e) {
 				System.out.println("Exc");
 				
-			} catch (IOException e) {
+			}catch (SocketTimeoutException e) {
+				System.out.println("Timeout");
+				
+			}catch (IOException e) {
 				
 			}
 		}
@@ -33,9 +39,54 @@ public class PortScanner {
 	public void TcpSYN(String ip) {
 		
 	}
+	public void UdpScan(String ip) {
+		for (int i=0;i<=10;i++) {
+			DatagramSocket ds;
+	        DatagramPacket dp;
+	        DatagramChannel dChannel;
+	        try{
+	            byte [] bytes = new byte[128];
+	            ds = new DatagramSocket();
+	            dp = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(ip),i);
+	            dChannel = DatagramChannel.open();
+	            dChannel.connect(new InetSocketAddress(ip, i));
+	            dChannel.configureBlocking(true);
+	            ds = dChannel.socket();
+	            ds.setSoTimeout(1000);
+	            ds.send(dp);
+	            dp = new DatagramPacket(bytes, bytes.length);
+	            Thread.sleep(1000);
+	            ds.receive(dp);
+
+	            if (!dChannel.isConnected() || !dChannel.isOpen()){
+	                ds.close();
+	            }
+	            System.out.println(Integer.toString(i) +" is open");
+	            String response = dp.toString();
+	            System.out.println(response);
+	            ds.disconnect();
+	            dChannel.disconnect();
+	            dChannel.close();
+	            ds.close();
+	        }
+	        catch(PortUnreachableException e){
+	        	  System.out.println( "port " + Integer.toString(i)+" is closed");
+	        }
+	        catch(InterruptedIOException e){
+	        	 System.out.println( "port " + Integer.toString(i)+" is closed");
+	        }
+	        catch(IOException e){
+	        	 System.out.println( "port " + Integer.toString(i)+" is closed");
+	        }
+	        catch(Exception e){
+	        	 System.out.println( "port " + Integer.toString(i)+" is closed");
+	        }
+	       
+		}
+	}
 	public void drawGUI() {
 		JFrame jf = new JFrame("Port Scanner");
-		jf.setSize(200,100);
+		jf.setSize(200,150);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel jp = new JPanel();
 		JButton tcpSyn = new JButton("TCP SYN Scan");
@@ -56,8 +107,18 @@ public class PortScanner {
 			  } 
 			  
 			} );
+		JButton udpScan = new JButton("UDP Scan");
+		udpScan.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) { 
+				  String ip = JOptionPane.showInputDialog("Enter IP address");
+				  UdpScan(ip);
+			   
+			  } 
+			  
+			} );
 		jp.add(tcpSyn);
 		jp.add(tcpConnect);
+		jp.add(udpScan);
 		jf.add(jp);
 		jf.setVisible(true);
 	}
