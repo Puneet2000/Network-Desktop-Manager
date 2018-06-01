b5926f95c02f090b3e0f5e74c7274e6a668797be
Puneet2000 <cs17btech11029@iith.ac.in>
2018-06-01 01:35:47 -0400

    TCP connect scan added

--- src/PortScanner.java
+++ src/PortScanner.java
@@ -1,23 +1,26 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.net.Socket;
 import java.net.UnknownHostException;
-
 import javax.swing.*;
-
+import java.io.*;
 public class PortScanner {
 
 	public static void main (String args[]) {
 		new PortScanner().drawGUI();
 	}
-	public void scan(String ip) {
+	public void TcpConnect(String ip) {
 		for (int i=0;i<=65535;i++) {
 			try {
 				Socket s = new Socket(ip,i);
-				System.out.println( ip +" is listening to port "+ Integer.toString(i));
+				System.out.println(ip+":"+i );
+				s.setSoTimeout(1000);
+				InputStream in = s.getInputStream();
+				BufferedInputStream bins = new BufferedInputStream(in);
+				BufferedReader bin = new BufferedReader(new InputStreamReader(bins));
+	            String response = bin.readLine();
+	            System.out.println(response);
 				s.close();
 			} catch (UnknownHostException e) {
 				System.out.println("Exc");
@@ -27,58 +30,34 @@ public class PortScanner {
 			}
 		}
 	}
-	public void services(String ip) {
-		for (int i=0;i<=65535;i++) {
-			try {
-				Socket s = new Socket(ip,i);
-				s.close();
-				 Process p1=Runtime.getRuntime().exec("grep -w "+i +" /etc/services"); 
-                 try {
-					p1.waitFor();
-					 BufferedReader reader=new BufferedReader(new InputStreamReader(p1.getInputStream())); 
-	                 String line=reader.readLine(); 
-	                 while(line!=null)  
-	                 { 
-	                    System.out.println(line); 
-	                    line=reader.readLine(); 
-	                 }
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} 
-                
-			} catch (UnknownHostException e) {
-				
-			} catch (IOException e) {
-				
-			}
-		}
+	public void TcpSYN(String ip) {
+		
 	}
 	public void drawGUI() {
 		JFrame jf = new JFrame("Port Scanner");
 		jf.setSize(200,100);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel jp = new JPanel();
-		JButton scan = new JButton("Scan");
-		scan.addActionListener(new ActionListener() { 
+		JButton tcpSyn = new JButton("TCP SYN Scan");
+		tcpSyn.addActionListener(new ActionListener() { 
 			  public void actionPerformed(ActionEvent e) { 
 				  String ip = JOptionPane.showInputDialog("Enter IP address");
-				  scan(ip);
+				  TcpSYN(ip);
 			   
 			  } 
 			  
 			} );
-		JButton services = new JButton("Running Services");
-		services.addActionListener(new ActionListener() { 
+		JButton tcpConnect = new JButton("TCP CONNECT Scan");
+		tcpConnect.addActionListener(new ActionListener() { 
 			  public void actionPerformed(ActionEvent e) { 
 				  String ip = JOptionPane.showInputDialog("Enter IP address");
-				  services(ip);
+				  TcpConnect(ip);
 			   
 			  } 
 			  
 			} );
-		jp.add(scan);
-		jp.add(services);
+		jp.add(tcpSyn);
+		jp.add(tcpConnect);
 		jf.add(jp);
 		jf.setVisible(true);
 	}
